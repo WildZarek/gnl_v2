f2a76236f460b1fc4bc9441dd7f25ac5497ba710
dsarmien <dsarmien@student.42malaga.com>
2024-11-29 16:49:27 +0100

    new branch to test

--- get_next_line.c
+++ get_next_line.c
@@ -6,44 +6,57 @@
 /*   By: dsarmien <dsarmien@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 17:35:29 by dsarmien          #+#    #+#             */
-/*   Updated: 2024/11/28 21:01:45 by dsarmien         ###   ########.fr       */
+/*   Updated: 2024/11/29 16:16:42 by dsarmien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char* read_buffer(int fd, char*  buffer);
+char*	read_buffer(int fd, char*  buffer);
 
 char	*get_next_line(int fd)
 {
-	char* line;
-	static char* buffer;
+	char	*line;
+	static char	*buffer;
 	
+	line = NULL;
 	if (BUFFER_SIZE <= 0 || fd < 0 || fd > MAX_FD)
 		return (NULL);
 	
-	buffer = malloc(BUFFER_SIZE + 1);
+	//buffer = malloc((BUFFER_SIZE + 1) * sizeof(char)) -- memory leak;
 	if (!buffer)
 		return (free(buffer), NULL);
 	buffer[BUFFER_SIZE] = '\0';
-	read(fd, buffer, BUFFER_SIZE);
+	// read(fd, buffer, BUFFER_SIZE) -- double read detected!;
 	line = read_buffer(fd, buffer);
-	printf("Pointer: %p\nContent: %s\n", line, line);
+	//free(buffer) -- should do this?;
+	printf("Pointer (to the line): %p ('%s')\n", line, line);
 	return(line);
 }
 
 char* read_buffer(int fd, char*  buffer)
 {
-		char* tmp;
-		ssize_t bytes_read;
+		char	*ptr;
+		char	*tmp;
+		ssize_t	bytes_read;
 		tmp = malloc((BUFFER_SIZE + 1) * sizeof(char));
 		if (!tmp) 
 			return(NULL);
 
 		bytes_read = read(fd, tmp, BUFFER_SIZE);
-		printf("Bytes read: %lu \nTemporary buffer: %s\n", bytes_read, tmp);
-		buffer = ft_strjoin(buffer, tmp);
-		printf("Static buffer: %s\n", buffer);
+		printf("Bytes read: %lu\n", bytes_read);
+		printf("Static Buffer (before initialize): %s\n", buffer);
+		if (!buffer)
+			buffer = tmp;
+		else
+		{
+			ptr = ft_strjoin(buffer, tmp);
+			free(ptr);
+			tmp = NULL;
+			if (!ptr)
+				return (NULL);
+		}
+		printf("Static Buffer (after initialize): %s\n", buffer);
 		free(tmp);
 		return(buffer);
 }
