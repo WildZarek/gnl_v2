f4b69ff0adff67080436883e40cdf767139aa7ed
dsarmien <dsarmien@student.42malaga.com>
2024-11-29 17:01:13 +0100

    multi reading main

--- main.c
+++ main.c
@@ -3,13 +3,29 @@
 
 int main(void)
 {
-	char* text = NULL;
-	int fd;
+	char	*text = NULL;
+	int		fd;
+
 	fd = open("tester", O_RDONLY);
 	if (fd == -1)
-		printf("Error reading");
-	else 
+	{
+		printf("Error reading!");
+		return (1);
+	}
+	else
+	{
+		// read 1
 		text = get_next_line(fd);
-	printf("%s", text);
+		printf("GNL Result: %s\n", text);
+		printf("\n");
+		// read 2
+		text = get_next_line(fd);
+		printf("GNL Result: %s\n", text);
+		printf("\n");
+		// read 3
+		text = get_next_line(fd);
+		printf("GNL Result: %s\n", text);
+		printf("\n");
+	}
+	return (0);
 }
-
