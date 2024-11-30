8558db52e9b567cbe56e34099dc9a21d273355a7
dsarmien <dsarmien@student.42malaga.com>
2024-11-30 18:56:45 +0100

    0123456789

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: dsarmien <dsarmien@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 17:35:29 by dsarmien          #+#    #+#             */
-/*   Updated: 2024/11/29 20:55:30 by dsarmien         ###   ########.fr       */
+/*   Updated: 2024/11/30 18:55:52 by dsarmien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char*	read_buffer(int fd, char*  buffer);
+char*	read_buffer(int fd, char *buffer);
 
 char	*get_next_line(int fd)
 {
@@ -25,23 +25,29 @@ char	*get_next_line(int fd)
 	
 /* 	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char)) //-- memory leak; */
 	if (!buffer)
+	{
 		buffer = ft_strdup("");
 		//return (free(buffer), NULL);
+	}
 	//buffer[BUFFER_SIZE] = '\0';
 	// read(fd, buffer, BUFFER_SIZE) -- double read detected!;
-	line = read_buffer(fd, buffer);
+	//line = read_buffer(fd, buffer);
+	line = get_one_line(buffer);
 	//free(buffer) -- should do this?;
-	printf("Pointer (to the line): %p ('%s')\n", line, line);
+	//printf("Line Pointer: %p -> %s\n", line, line);
+	buffer = NULL;
 	return(line);
 }
 
-char* read_buffer(int fd, char*  buffer)
+char	*read_buffer(int fd, char *buffer)
 {
 		char	*aux_ptr;
 		char	*read_data;
 		ssize_t	bytes_read;
+		size_t	count;
 
 		bytes_read = 1;
+		count = 0;
 		while (!(ft_strchr(read_data, '\n')) && bytes_read > 0)
 		{
 			read_data = malloc((BUFFER_SIZE + 1) * sizeof(char));
@@ -49,7 +55,7 @@ char* read_buffer(int fd, char*  buffer)
 				return(NULL);
 			
 			bytes_read = read(fd, read_data, BUFFER_SIZE);
-			printf("Bytes read: %lu\n", bytes_read);
+			printf("\nBytes read: %lu | Iteration (read): %zu\n", bytes_read, count + 1);
 			printf("Static Buffer (before initialize): %s\n", buffer);
 
 			aux_ptr = buffer;
@@ -67,6 +73,7 @@ char* read_buffer(int fd, char*  buffer)
 			free(read_data);
 			read_data = NULL;
 			free(aux_ptr);
+			count++;
 		}
 		return(buffer);
 }
